d3b5b32561229bd00acfd829469cc144e3ed0da8
ben-tsai-kou <dragon7230@gmail.com>
2024-09-14 10:37:53 +0900

    HTTPS with nodes, self signed certificates, and public key cryptography

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+const fs = require('fs');
 const path = require('path');
+const https = require('https');
 const express = require('express');
 
 const PORT = process.env.PORT || 3000;
@@ -12,6 +14,14 @@ app.get('/', (req, res) => {
   return res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+https
+  .createServer(
+    {
+      cert: fs.readFileSync(path.join(__dirname, 'cert.pem')),
+      key: fs.readFileSync(path.join(__dirname, 'key.pem')),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
