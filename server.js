b5765d68534dc1cbbcc1096f39cff69c56443890
ben-tsai-kou <dragon7230@gmail.com>
2024-09-15 10:59:29 +0900

    install helmet.js

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 const https = require('https');
+
+const helmet = require('helmet');
 const express = require('express');
 
 const PORT = process.env.PORT || 3000;
+
 const app = express();
+
+app.use(helmet());
 
 app.get('/secret', (req, res) => {
   return res.send('Your personal secret value is 42');
